85fa36d2db17dc89a0f25e56ba8cb01b2d820353
v_sunxiaoxu <sunxiaoxu0124@163.com>
2016-10-31 16:21:32 +0800

    commit1

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,12 +3,12 @@ package test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.demo.entity.Reward;
+import com.demo.entity.Activity;
 
 public class Test {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/demo/resource/SpringFramework.xml");
-		Reward reward = context.getBean("reward", Reward.class);
-		System.out.println(reward);
+		Activity activity = context.getBean("activity", Activity.class);
+		System.out.println(activity);
 	}
 }
